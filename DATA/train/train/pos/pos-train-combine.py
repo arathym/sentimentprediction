b0c804c889f0f28e9332f22d06a34102a2e9c65e
Arathy Mohan <arathy.gmohan@gmail.com>
2016-05-04 22:19:08 -0400

    Adding positive training data

---
+++ DATA/train/train/pos/pos-train-combine.py
@@ -0,0 +1,18 @@
+#----- combining all the text files into the csv and also removing the punctuations------
+# This program should be run inside the pos folder since we are taking all the global .text files from that folder
+
+import glob 
+import string
+
+fw = open('pos-train.txt','wb') # file to which we are writing all the text files to
+punc = string.punctuation # has all the punctuations
+
+for files in glob.glob("*.txt"):  # this searches all the pathnames(like .csv,.txt) provided in the folder        
+       fr = open(files,'rb') # opening the file
+       data = fr.readline() # removing the punctuations and converting all words to lower letters          
+       fw.writelines(data + '\n') # writing the translated data to the csv file and adding new line at the end    
+       
+fw.close() # closing the file to which we wrote
+
+
+
